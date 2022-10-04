c9f064dc0f50a75c56b44d8159b9da55a24bbc38
Simron <simron0707@gmail.com>
2022-10-04 22:22:41 +0530

    routing done and some modification in login form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 // import logo from './logo.svg';
+import { memo } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import "./App.css";
 import LoginContainer from "./Components/login/LoginContainer.js";
 import Home from "./Components/home/Home";
 import FormContainer from "./Components/apprenticeship_form/FormContainer";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <LoginContainer /> */}
-      {/* <Home /> */}
-      <FormContainer />
-    </div>
-  );
+	<LoginContainer />
+	return (
+		<div className="App">
+			<Routes>
+				<Route
+					path={'/*'}
+					element={
+						<Home />
+					}
+				/>
+				<Route
+					path={'/login'}
+					element={
+						<LoginContainer />
+					}
+				/>
+				<Route
+					path={'/apprenticeship-profile'}
+					element={
+						<FormContainer />
+					}
+				/>
+			</Routes>
+		</div>
+	);
 }
 
-export default App;
+export default memo(App);
--- src/Components/login/LoginForm.js
+++ src/Components/login/LoginForm.js
@@ -1,28 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import radicalx_icon from "../../assests/images/radicalx_logo.png";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import InputAdornment from '@mui/material/InputAdornment';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import Input from '@mui/material/Input';
+
 
 function LoginForm() {
+  /*Email */
+  const [email, setEmail] = useState("");
+
+  /* Password */
+  const [values, setValues] = React.useState({
+    password: "",
+    showPassword: false,
+  });
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handlePasswordChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
   return (
     <div className="subContainer2">
       <div className="logo">
         <img src={radicalx_icon} alt="" />
       </div>
-
       <div className="form">
         <div className="title">Login</div>
-        <div className="emailBox">
-          <EmailOutlinedIcon style={{ color: "#8A8B8C" }} />
-          <input type="email" placeholder="Email" />
-        </div>
-
-        <div className="passwordBox">
-          <LockOutlinedIcon style={{ color: "#8A8B8C" }} />
-          <input type="password" placeholder="Password" />
-          <VisibilityOutlinedIcon style={{ color: "#8A8B8C" }} />
-        </div>
+        <Input disableUnderline 
+        className="emailBox" 
+        placeholder="Email"
+        type="email"
+        value={email}
+        onChange={event => setEmail(event.target.value)}
+        startAdornment={
+            <EmailOutlinedIcon position="start" style={{ color: "#8A8B8C", marginRight: "9px" }} />
+          }
+        />
+        <Input disableUnderline 
+        className="passwordBox"
+        placeholder="Password"
+        type={values.showPassword ? "text" : "password"}
+        onChange={handlePasswordChange("password")}
+        value={values.password}
+        startAdornment={
+            <LockOutlinedIcon position="start" style={{ color: "#8A8B8C", marginRight: "9px" }} />
+          }
+        endAdornment={
+            <InputAdornment position="end">
+              <div
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {values.showPassword ? <VisibilityIcon style={{ color: "#8A8B8C", cursor: "pointer" }} /> : <VisibilityOffIcon style={{ color: "#8A8B8C", cursor: "pointer" }} />}
+              </div>
+            </InputAdornment>
+          }
+        />
 
         <div className="forgotPassword">
           <div className="rememberMe">
